8b8f30e03ee0b8da699cf963f71ccde7f7d0cb41
douglas <douglasmaxton@gmail.com>
2025-02-10 17:38:06 +0000

    feat(created): add new task button

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import DropdownList from "./components/molecule/DropdownList.jsx";
 
 function App() {
   return (
-    <>
-      <div className="flex w-full justify-center h-screen bg-gray-100">
-        <div className="block justify-between">
-          <SearchBar />
+    <div className="flex w-full justify-center h-screen bg-gray-100">
+      <div className="block justify-between">
+        {/*search bar with icon*/}
+        <SearchBar />
+
+        {/*dropdown lists for sort and category*/}
+        <div className="flex">
           <DropdownList
             title={"Sort"}
             itemsList={[
@@ -23,8 +26,28 @@ function App() {
             itemsList={["Education", "Career", "Job", "Success", "Routine"]}
           />
         </div>
+
+        {/*add new task button*/}
+        <button className="flex space-x-3 w-75 m-auto justify-center items-center px-5 py-3 bg-indigo-500 hover:bg-indigo-800 rounded-full drop-shadow-md cursor-pointer duration-300">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+
+          <span className="text-white text-xl font-bold">Add New Task</span>
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
